a5fd228322d896ed5cafa783eacd11cfc1ff2820
mikeyo4800 <mikeyo4800@gmail.com>
2022-06-29 21:46:30 -0400

    updated

---
+++ app.py
@@ -0,0 +1,82 @@
+import joblib
+import streamlit as st
+from preprocessing import TextPreprocessor
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+
+# 1 . button to load in pdf 
+
+st.title("Screenplay Genre Classifier")
+st.subheader("In this application, we use a classifier chain with a base estimator to classifiy the genre of our screenplay.")
+
+uploaded_file = st.file_uploader("Upload a file (txt file only for now)")
+
+
+lst = []
+
+if uploaded_file:
+     
+     for line in uploaded_file:
+          lst.append(line.decode())
+
+     data = " ".join(lst)
+
+# 2. convert api from pdf to text
+
+# 3. Text preprocessing (lemmatizing and removing stop words)
+     
+     stop_words = []
+
+     with open('stop_list.txt', 'r') as fp:
+          for line in fp:
+               x = line[:-1]
+
+        # add current item to the list
+               stop_words.append(x)
+     
+     with open('names.txt', 'r') as f:
+          for line in f:
+               
+               y = line[:-1]
+
+        # add current item to the list
+               stop_words.append(y.lower())
+
+     tp = TextPreprocessor(activator_type='wnl', lem_or_stem='lem')
+     
+     data_lemmatized = tp.lem_process_doc(data)
+     
+     data_cleaned = " ".join([text for text in data_lemmatized.split() if text not in stop_words])
+
+     
+
+# 4. Word Cloud?
+     plt.figure(figsize=(40,25))
+     cloud = WordCloud(
+                              background_color='black',
+                              collocations=True,
+                              width=2500,
+                              height=1800
+                              ).generate(data_cleaned)
+     plt.axis('off')
+     plt.title('Word Cloud of Script',fontsize=40)
+     plt.imshow(cloud)
+     st.set_option('deprecation.showPyplotGlobalUse', False)
+     st.pyplot()
+
+
+# 5. model predictions
+     with open("model_genre.pkl", "rb") as f:
+          model = joblib.load(f)
+
+     predictions = model.predict([data_cleaned])
+
+     genre_names = []          
+     genre_dct = {0 : 'Crime', 1:'Romance', 2: 'Animation', 3: 'SciFi', 4: 'Fantasy', 5: 'History', 6: 'Action', 7: 'Drama', 8:'War', 9: 'Thriller', 10: 'Mystery', 11: 'Documentary', 12: 'Horror', 13: 'Family', 14: 'Adventure', 15: 'Music', 16: 'Comedy', 17: 'Western'}
+
+     for i,x in enumerate(predictions[0]):
+          if x == 1:
+               genre_names.append(genre_dct[i])
+
+     st.write(genre_names)
---
+++ preprocessing.py
@@ -0,0 +1,144 @@
+#importing necessary packages
+from sklearn.base import BaseEstimator, TransformerMixin
+import itertools
+
+import pandas as pd
+import numpy as np
+from nltk.corpus import stopwords
+import re
+from nltk.tokenize import word_tokenize
+from nltk.corpus import wordnet
+from nltk import pos_tag
+from nltk import WordNetLemmatizer
+
+from nltk.stem import PorterStemmer, LancasterStemmer
+from nltk.stem.snowball import SnowballStemmer
+
+
+
+
+class TextPreprocessor(BaseEstimator, TransformerMixin): #for pipeline and column transformation (not used in this project)
+    
+    def __init__(self, activator_type = None, lem_or_stem = None, stop_words = None):
+        
+        """
+        This class object takes three arguments: activator_type, lem_or_stem, and stop_words.
+
+        activator_type is for the type of stemming or lemming process to be applied to the text.
+            options: ps- PorterStemmer, ss- Snowball Stemmer, ls- Lancaster Stemmer- wnl - WordNetLemmatiztion, tb- TextBlob, pt - Pattern
+
+        lem_or_stem indicates whether to lemmatize or stem the text data
+
+        stop_words is if you wish to add a custom stop words list to be used in the textpreprocessing transformation
+
+        All arguments are set to None.
+
+        *Will throw up an error if activator_type indicates stemming while lem_or_stem is lemming and vice versa
+        
+        """
+
+
+
+        self.activator_type = activator_type
+        self.lem_or_stem = lem_or_stem
+        self.stop_words = stop_words
+    
+    def fit(self, data, y = 0):
+        return self
+    
+    def transform(self, data, y = 0):
+        
+        if self.lem_or_stem == 'lem':
+            fully_normalized_corpus = data.apply(self.lem_process_doc)
+            return fully_normalized_corpus
+        
+        elif self.lem_or_stem == 'stem':
+            fully_normalized_corpus = data.apply(self.stem_process_doc)
+            return fully_normalized_corpus
+
+    def the_cleaner(self, text):
+        
+        text_replace = text.replace('\\r', ' ').replace('\\n', ' ').replace('\\', '').split()
+        text_strip = [re.sub(r"\([^()]*\)", "", i) for i in text_replace]
+        perfect = " ".join([x for x in text_strip if x.isalpha() and len(x) > 1])
+
+        return perfect
+    
+    def lem_process_doc(self, doc):
+
+
+        stop_words = stopwords.words('english')
+        movie_stop_words = ['b', 'fade', 'in', 'cut', 'to', 'int', 'ext', 'v', 'o', '\b', 'out', 'transition', 'to', 'angle', 'pan', 'word', 'title', 'description', 'screenplay']
+        stop_words_modified = list(itertools.chain(movie_stop_words, stop_words))
+        doc_lower = self.the_cleaner(doc).lower()
+
+
+        if self.stop_words != None:
+            stop_words_modified = list(itertools.chain(stop_words_modified, self.stop_words))
+        
+        if self.activator_type == 'wb':
+            
+            sent = TextBlob(doc_lower)
+            tag_dict = {"J": 'a', "N": 'n', "V": 'v', "R": 'r'}
+            words_and_tags = [(w, tag_dict.get(pos[0], 'n')) for w, pos in sent.tags]    
+            lemmatized_list = [wd.lemmatize(tag) for wd, tag in words_and_tags]
+            return " ".join(lemmatized_list)
+
+
+        elif self.activator_type == 'pt':
+            
+            doc_norm = [tok for tok in word_tokenize(doc_lower) if ((tok.isalpha()) & (tok not in stop_words_modified))]
+            return " ".join([lemma(tok) for tok in doc_norm])
+
+        elif self.activator_type == 'wnl':
+            wnl = WordNetLemmatizer()
+
+            def pos_tagger(nltk_tag):
+                if nltk_tag.startswith('J'):
+                    return wordnet.ADJ
+                elif nltk_tag.startswith('V'):
+                    return wordnet.VERB
+                elif nltk_tag.startswith('N'):
+                    return wordnet.NOUN
+                elif nltk_tag.startswith('R'):
+                    return wordnet.ADV
+                else:         
+                    return None
+
+
+            doc_norm = [tok for tok in word_tokenize(doc_lower) if ((tok.isalpha()) & (tok not in stop_words_modified)) ]
+
+
+            wordnet_tagged = list(map(lambda x: (x[0], pos_tagger(x[1])), pos_tag(doc_norm))) 
+            doc_norm = [wnl.lemmatize(token, pos) for token, pos in wordnet_tagged if pos is not None]
+
+            return " ".join(doc_norm)
+
+    def stem_process_doc(self, doc):
+
+        stop_words = stopwords.words('english')
+        movie_stop_words = ['b', 'fade', 'in', 'cut', 'to', 'int', 'ext', 'v', 'o', '\b', 'out', 'transition', 'to', 'angle', 'pan', 'word', 'title', 'description', 'screenplay']
+        stop_words_modified = list(itertools.chain(movie_stop_words, stop_words))
+        doc_lower = self.the_cleaner(doc).lower()
+        
+        if self.stop_words != None:
+            stop_words_modified = list(itertools.chain(stop_words_modified, self.stop_words))
+        
+        doc_norm = [tok for tok in word_tokenize(doc_lower) if ((tok.isalpha()) & (tok not in stop_words_modified))]
+
+        if self.activator_type == 'ps':
+
+            ps = PorterStemmer()
+            stem_doc = [ps.stem(x) for x in doc_norm]
+            return " ".join(stem_doc)
+
+        elif self.activator_type == 'ls':
+
+            ls = LancasterStemmer()
+            stem_doc = [ls.stem(x) for x in doc_norm]
+            return " ".join(stem_doc)
+
+        elif self.activator_type == 'ss':
+            sn = SnowballStemmer('english')
+            stem_doc = [sn.stem(x) for x in doc_norm]
+            return " ".join(stem_doc)
